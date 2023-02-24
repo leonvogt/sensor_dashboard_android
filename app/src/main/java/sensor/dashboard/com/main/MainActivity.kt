2c28cd0db8b24374bc655cb97411d336fc491394
Leon <nonick@nonick.ch>
2023-02-24 21:29:11 +0100

    Add bottom navigation menu

--- app/src/main/java/sensor/dashboard/com/main/MainActivity.kt
+++ app/src/main/java/sensor/dashboard/com/main/MainActivity.kt
@@ -2,17 +2,45 @@ package sensor.dashboard.com.main
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import dev.hotwire.turbo.activities.TurboActivity
 import dev.hotwire.turbo.delegates.TurboActivityDelegate
 import sensor.dashboard.com.R
+import sensor.dashboard.com.databinding.ActivityMainBinding
+import sensor.dashboard.com.util.DEVICES_URL
+import sensor.dashboard.com.util.HOME_URL
 
 class MainActivity : AppCompatActivity(), TurboActivity {
     override lateinit var delegate: TurboActivityDelegate
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navView: BottomNavigationView = binding.navView
+        navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
 
         delegate = TurboActivityDelegate(this, R.id.main_nav_host)
+    }
+
+    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+            R.id.navigation_home -> {
+                delegate.navigate(HOME_URL)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_devices -> {
+                delegate.navigate(DEVICES_URL)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_notifications -> {
+                delegate.navigate(HOME_URL)
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+        false
     }
 }
--- app/src/main/java/sensor/dashboard/com/util/Constants.kt
+++ app/src/main/java/sensor/dashboard/com/util/Constants.kt
@@ -2,4 +2,5 @@ package sensor.dashboard.com.util
 
 const val BASE_URL = "https://sensor-dashboard.dev-box.ch"
 const val HOME_URL = BASE_URL
+const val DEVICES_URL = "$BASE_URL/devices"
 const val SIGN_IN_URL = "$BASE_URL/users/sign_in"
